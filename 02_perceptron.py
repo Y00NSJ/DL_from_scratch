e9220776f30d22f90cf2af0391eab8259a130dff
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-10-08 03:03:15 +0900

    feat: implement perceptron = 'XOR'

--- 02_perceptron.py
+++ 02_perceptron.py
@@ -30,21 +30,29 @@ def OR(x1, x2):
     weighted_sum = np.sum(w * x) + b
     return 1 if weighted_sum > 0 else 0
 
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y  = AND(s1, s2)
+    return y
+
 
 inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
 truth_table = np.array(inputs)
-header = np.array([["x1", "x2", "AND", "NAND", "OR"]])
+header = np.array([["x1", "x2", "AND", "NAND", "OR", "XOR"]])
 
 and_outputs = []
 nand_outputs = []
 or_outputs = []
+xor_outputs = []
 for row in inputs:
     x1, x2 = row
     and_outputs.append(AND_bias(x1, x2))
     nand_outputs.append(NAND(x1, x2))
     or_outputs.append(OR(x1, x2))
+    xor_outputs.append(XOR(x1, x2))
 
-truth_table = np.c_[truth_table, and_outputs, nand_outputs, or_outputs]
+truth_table = np.c_[truth_table, and_outputs, nand_outputs, or_outputs, xor_outputs]
 truth_table = np.vstack([header, truth_table.astype(str)])
 
 for row in truth_table:
