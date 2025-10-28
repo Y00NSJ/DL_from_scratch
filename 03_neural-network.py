644478817919142180f4961cbc0edb23cc92bc43
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-10-28 22:20:06 +0900

    feat: implement step f. for numpy array

--- 03_neural-network.py
+++ 03_neural-network.py
@@ -1,3 +1,7 @@
-# 계단 함수 구현
+## 계단 함수 구현
 def step_function(x):
     return 1 if x > 0 else 0
+# numpy 배열에 대응하도록 수정
+def step_function_numpy(x):
+    y = x > 0               # 입력 numpy 배열의 각 원소에 대해 Bool 값 대응
+    return y.astype(int)    # Bool -> Int로 변환된 numpy 배열로 변환
