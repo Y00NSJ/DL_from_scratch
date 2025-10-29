2e03d38840e5c64f37701deb145c901f3d49cddb
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-10-29 20:31:14 +0900

    feat: implement sigmoid function

--- 03-neural_network/activate_functions.py
+++ 03-neural_network/activate_functions.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
 ## 계단 함수 구현
 def step_function(x):
     return 1 if x > 0 else 0
@@ -5,17 +9,25 @@ def step_function(x):
 def step_function_numpy(x):
     y = x > 0               # 입력 numpy 배열의 각 원소에 대해 Bool 값 대응
     return y.astype(int)    # Bool -> Int로 변환된 numpy 배열로 변환
+
 # 도식화
-import numpy as np
-import matplotlib.pylab as plt
-
-
 def step_function_plt(x):
     return np.array(x > 0, dtype=int)
 
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function_plt(x)
 
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+
+## 시그모이드 함수 구현
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# 도식화
+y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
